aa7d4586f0947f5a99da8c18e3e60dc60b4033c2
KapilG00 <kapil.gupta4949@gmail.com>
2026-09-14 16:46:14 +0530

    infix to postfix practive

--- Stack & Queues/Easy/temp.py
+++ Stack & Queues/Easy/temp.py
@@ -1,18 +1,62 @@
 # TC: O(n)
 # SC: O(N)
-def number_of_greater_elements_to_right(arr: List[int]) -> List[int]:
-    n = len(arr)
-    greater_ele_arr = [-1] * n
+def infix_to_postfix(infix_string: str) -> str:
+    n = len(infix_string)
     stack = []
+    res = ""
+    i = 0
 
-    for i in range(n):
-        while stack and arr[i] > arr[stack[-1]]:
-            poped_idx = stack.pop()
-            greater_ele_arr[poped_idx] = arr[i]
-        stack.append(i)
+    operators_priorities_mapping = {
+        "+": 1,
+        "-": 1,
+        "*": 2,
+        "/": 2,
+        "^": 3,
+        "(": 0,
+        ")": 0,
+    }
 
-    return greater_ele_arr
+    while i < n:
+        # Adding the operands to the result.
+        if infix_string[i].isalnum():
+            res += infix_string[i]
 
+        # Handling the "(" and ")" conditions.
+        elif infix_string[i] == "(":
+            stack.append(infix_string[i])
+
+        elif infix_string[i] == ")":
+            while len(stack) > 0 and stack[-1] != "(":
+                res += stack.pop()
+            # This pop is to remove the "(" from the stack.
+            stack.pop()
+
+        # Adding the operators to the stack.
+        else:
+            while (
+                len(stack) > 0
+                and stack[-1] != "("
+                and (
+                    operators_priorities_mapping[infix_string[i]]
+                    < operators_priorities_mapping[stack[-1]]
+                    or (
+                        operators_priorities_mapping[infix_string[i]]
+                        == operators_priorities_mapping[stack[-1]]
+                        and infix_string[i] != "^"
+                    )
+                )
+            ):
+                res += stack.pop()
+
+            stack.append(infix_string[i])
+
+        i += 1
+
+    # Adding remaining elements from stack into postfix string.
+    while len(stack) > 0:
+        res += stack.pop()
+
+    return res
 
 
 if __name__ == "__main__":
